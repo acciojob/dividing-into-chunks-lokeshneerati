3d9484264d081fc9b7db48afe8ba8f60a57691a2
lokeshneerati <lokeshneerati84@gmail.com>
2024-06-22 16:40:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
-  // Write your code here
+	let result = [];
+	let currentChunk = []
+	let currentSum = 0;
+	for(i = 0; i < arr.length; i++) {
+		if(currentSum + arr[i] <=n) {
+			currentChunk.push(arr[i]);
+			currentSum += arr[i];
+		} else {
+			result.push(currentChunk);
+			// starting a new chunk with current element
+			currentSum = arr[i];
+			currentChunk = [arr[i]];
+		}
+	}
+	result.push(currentChunk);
+	return result;
 };
 
-const n = prompt("Enter n: ");
-alert(JSON.stringify(divide(arr, n)));
+// const n = prompt("Enter n: ");
+// alert(JSON.stringify(divide(arr, n)));
